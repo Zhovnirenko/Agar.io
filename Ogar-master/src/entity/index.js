535b0cab3cb298027b861fc7dcf1cb6f1ef73f9e
Zhovnirenko <Zhovnirenka@gmail.com>
2025-01-19 02:45:24 +0200

    Initial commit

---
+++ Ogar-master/src/entity/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    Cell: require('./Cell'),
+    PlayerCell: require('./PlayerCell'),
+    Food: require('./Food'),
+    RareFood: require('./RareFood'),
+    Virus: require('./Virus'),
+    MotherCell: require('./MotherCell'),
+    EjectedMass: require('./EjectedMass'),
+};
